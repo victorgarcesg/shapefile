1de2f56f2f012c41a55f9c8479ac1c61f6c8b078
Victor Garces <victorgarcesgg@gmail.com>
2019-04-05 14:16:32 -0400

    Fixed bug with colors

--- shapefile/src/app/app.component.ts
+++ shapefile/src/app/app.component.ts
@@ -17,14 +17,83 @@ import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
 })
 export class AppComponent implements OnInit {
   title = 'shapefile';
+  map: Map;
 
   ngOnInit() {
-
     const image = new CircleStyle({
       radius: 5,
       fill: null,
       stroke: new Stroke({ color: 'red', width: 1 })
     });
+
+    const styles = {
+      'Point': new Style({
+        image: image
+      }),
+      'LineString': new Style({
+        stroke: new Stroke({
+          color: 'green',
+          width: 1
+        })
+      }),
+      'MultiLineString': new Style({
+        stroke: new Stroke({
+          color: 'green',
+          width: 1
+        })
+      }),
+      'MultiPoint': new Style({
+        image: image
+      }),
+      'MultiPolygon': new Style({
+        stroke: new Stroke({
+          color: 'yellow',
+          width: 1
+        }),
+        fill: new Fill({
+          color: 'rgba(255, 255, 0, 0.1)'
+        })
+      }),
+      'Polygon': new Style({
+        stroke: new Stroke({
+          color: 'blue',
+          lineDash: [4],
+          width: 3
+        }),
+        fill: new Fill({
+          color: 'rgba(0, 0, 255, 0.1)'
+        })
+      }),
+      'GeometryCollection': new Style({
+        stroke: new Stroke({
+          color: 'magenta',
+          width: 2
+        }),
+        fill: new Fill({
+          color: 'magenta'
+        }),
+        image: new CircleStyle({
+          radius: 10,
+          fill: null,
+          stroke: new Stroke({
+            color: 'magenta'
+          })
+        })
+      }),
+      'Circle': new Style({
+        stroke: new Stroke({
+          color: 'red',
+          width: 2
+        }),
+        fill: new Fill({
+          color: 'rgba(255,0,0,0.2)'
+        })
+      })
+    };
+
+    const styleFunction = (feature) => {
+      return styles[feature.getGeometry().getType()];
+    };
 
     const geojsonObject = {
       'type': 'FeatureCollection',
@@ -101,11 +170,14 @@ export class AppComponent implements OnInit {
       features: (new GeoJSON()).readFeatures(geojsonObject)
     });
 
+    vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)));
+
     const vectorLayer = new VectorLayer({
-      source: vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)))
+      source: vectorSource,
+      style: styleFunction
     });
 
-    const map = new Map({
+    this.map = new Map({
       layers: [
         new TileLayer({
           source: new OSM()
