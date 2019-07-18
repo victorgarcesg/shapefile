38bc38f5f394e986f9e87390a09ca819e55767b7
Victor Garces <victorgarcesgg@gmail.com>
2019-07-18 15:59:58 -0400

    Added functionality to know province by click

--- shapefile/src/app/app.component.ts
+++ shapefile/src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import GeoJSON from 'ol/format/GeoJSON';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import * as turf from '@turf/turf';
 import Map from 'ol/Map';
 import 'ol/ol.css';
-import { OSM, Vector as VectorSource } from 'ol/source';
-import View from 'ol/View';
 import * as shp from 'shpjs';
-import { loadshp } from '../../src/machuque/preview.js';
 declare var L;
 declare var ol: any;
 
@@ -19,51 +15,63 @@ export class AppComponent implements OnInit {
   title = 'shapefile';
   map: Map;
   currentGeoJson: any;
+  selectedPolygon: any;
   index = 0;
+  switch = false;
 
   async ngOnInit() {
-    // await shp('assets/reg.zip').then(geoJson => {
-    //   console.log(geoJson);
-    //   this.currentGeoJson = geoJson;
-    // });
-
-    // loadshp({
-    //   url: 'assets/reg.zip', // path or your upload file
-    //   encoding: 'utf-8', // default utf-8
-    //   EPSG: 4326 // default 4326
-    // }, (geojson) => {
-    //   console.log(geojson);
-    //   vector.addData(geojson);
-    //   map.fitBounds(vector.getBounds());
-    // });
-
-    //////////// LEAFTLEFT /////////////////////////
-    const map = L.map('map').setView([18.476466668402324, -69.91353750228882], 16);
-
+    const map = L.map('map').setView([18.476466668402324, -69.91353750228882], 8);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    shp('assets/REGCenso2010').then(geoJson => {
-      L.geoJSON(geoJson, {
-        onEachFeature: this.onEachFeature
-      }).addTo(map);
+    map.on('click', (e) => {
+      this.selectedPolygon = null;
+      const coord = e.latlng;
+      const lat = coord.lat;
+      const lng = coord.lng;
+      const point = turf.point([lng, lat]);
+      this.currentGeoJson.features.forEach(feature => {
+        if (feature.geometry.coordinates.length > 1) {
+          const multiPolygon = turf.multiPolygon(feature.geometry.coordinates);
+          if (turf.booleanPointInPolygon(point, multiPolygon)) {
+            this.selectedPolygon = feature;
+          }
+        } else {
+          const parentPolygon = turf.polygon(feature.geometry.coordinates);
+          if (turf.booleanPointInPolygon(point, parentPolygon)) {
+            this.selectedPolygon = feature;
+          }
+        }
+      });
+      if (this.selectedPolygon !== null) {
+        console.log(this.selectedPolygon.properties);
+        L.popup().setLatLng(coord).setContent(this.selectedPolygon.properties.TOPONIMIA).openOn(map);
+      }
     });
 
-    // loadshp({
-    //   url: 'assets/reg.zip', // path or your upload file
-    //   encoding: 'utf-8', // default utf-8
-    //   EPSG: 4326 // default 4326
-    // }, (geoJson) => {
-    //   L.geoJSON(geoJson, {
-    //     onEachFeature: this.onEachFeature
-    //   }).addTo(map);
-    // });
-
+    shp('assets/BPCenso2010').then(geoJson => {
+      console.log(geoJson);
+      this.currentGeoJson = geoJson;
+      // L.geoJSON(geoJson, {
+      //   onEachFeature: this.onEachFeature
+      // }).addTo(this.map);
+    });
   }
 
   private onEachFeature(feature, layer) {
-    console.log(feature.properties);
     layer.bindPopup(feature.properties.TOPONIMIA);
+  }
+
+  async printJson() {
+    let locations = [];
+    await shp('assets/BPCenso2010').then(geoJson => {
+      console.log(geoJson);
+      geoJson.features.forEach(feature => {
+        locations = [...locations, feature.properties];
+      });
+    });
+    const str = JSON.stringify(locations, null, 2);
+    //console.log(str);
   }
 }
